5ccb7e62247aec6166234ebb05bb156de17a96fd
waniyah_khan <khanwaniyah321@GMAIL.COM>
2025-04-20 00:32:54 +0500

    Add files via upload

---
+++ lift_off/file.py
@@ -0,0 +1,9 @@
+def main():
+    # Countdown from 10 to 1
+    for i in range(10, 0, -1):  # Start at 10, go down to 1 
+        print(i, end=' ')  # Print the current number
+    
+    print("Liftoff!")  # after the countdown
+
+if __name__ == '__main__':
+    main()
